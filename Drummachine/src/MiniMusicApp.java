b85122c325271470c61f7877640cfc5806a1fc6a
Kirill <k.kozhuhovskiy@gmail.com>
2015-12-04 17:06:06 +0200

    add class musicCmd

--- Drummachine/src/MiniMusicApp.java
+++ Drummachine/src/MiniMusicApp.java
@@ -15,23 +15,23 @@ public class MiniMusicApp {
             Track track = seq.createTrack();
 
             ShortMessage a = new ShortMessage();
-            a.setMessage(144, 1 , 44, 100);
-            MidiEvent noteOn = new MidiEvent(a, 1);
+            a.setMessage(144, 1 , 14, 100);
+            MidiEvent noteOn = new MidiEvent(a, 2);
             track.add(noteOn);
 
             ShortMessage b = new ShortMessage();
-            b.setMessage(128, 1, 44, 100);
-            MidiEvent noteOff = new MidiEvent(b, 16);
+            b.setMessage(192, 1, 34, 100);
+            MidiEvent noteOff = new MidiEvent(b, 6);
             track.add(noteOff);
 
             ShortMessage c = new ShortMessage();
-            c.setMessage(128, 1, 44, 100);
-            MidiEvent midiEvent = new MidiEvent(c, 12);
+            c.setMessage(128, 1, 24, 100);
+            MidiEvent midiEvent = new MidiEvent(c, 3);
             track.add(midiEvent);
 
             ShortMessage ab = new ShortMessage();
-            a.setMessage(144, 1 , 44, 100);
-            MidiEvent noteOb = new MidiEvent(ab, 1);
+            a.setMessage(134, 1, 102, 100);
+            MidiEvent noteOb = new MidiEvent(ab, 2);
             track.add(noteOb);
 
             player.setSequence(seq);
